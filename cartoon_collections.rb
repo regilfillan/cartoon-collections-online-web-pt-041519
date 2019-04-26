5ac643724f6e28852c9e5f8fd4e33bf2a600a88d
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 22:01:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  if array.length 
   # Your code here
 end
 
