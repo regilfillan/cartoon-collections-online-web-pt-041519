8d9dfeac3c92b02b4e3006a8616341b4296f8d61
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 22:12:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |string|
-    string.include?(cheese_types)
+    cheese_types.include?(string)
   end
 end
